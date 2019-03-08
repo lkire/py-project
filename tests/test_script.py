7154d351e16c42658fc42fa554364396fce4366d
lkire <alkire@gmail.com>
2019-03-08 23:59:48 +0000

    added tests

---
+++ tests/test_script.py
@@ -0,0 +1,8 @@
+
+from project.script import printscript, script_generator
+def test_printscript():
+    assert printscript() == 'script'
+
+
+def test_script_generator():
+    assert ['script','script'] == [s for s in script_generator(2)]
