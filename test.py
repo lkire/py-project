29873a26ead3921293f7d7ee2890862b53d999a4
lkire <alkire@gmail.com>
2019-03-11 21:37:16 +0000

    switched to flake8

--- test.py
+++ test.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
 from subprocess import call
-#intentionally blocking otherwise use popen
+# intentionally blocking otherwise use popen
 from glob import glob
 
 SRC_DIR = "project"
 
+
 def test():
-    call_str = ["python","-m","pytest"]
+    call_str = ["python", "-m", "pytest"]
     call(call_str)
+
 
 def pep():
-    call_str = ["autopep8","--in-place", "--aggressive", "--aggressive"]
+    call_str = ["autopep8", "--in-place", "--aggressive", "--aggressive"]
     call_str.extend(glob(f"{SRC_DIR}/*.py"))
+    call_str.extend(glob(f"tests/*.py"))
+    call_str.extend(glob(f"*.py"))
     call(call_str)
 
-def lint():
-    call_str = ["pylint", f"{SRC_DIR}"]
 
-if __name__== "__main__":
+def lint():
+    call_str = ["python", "-m", "flake8"]
+    call(call_str)
+
+
+if __name__ == "__main__":
     test()
     pep()
     lint()
