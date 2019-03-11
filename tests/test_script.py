adbca628f1086c488c41a2d5555eade3f780df9c
lkire <alkire@gmail.com>
2019-03-11 21:37:30 +0000

    linted

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,8 +1,10 @@
 
 from project.script import printscript, script_generator
+
+
 def test_printscript():
     assert printscript() == 'script'
 
 
 def test_script_generator():
-    assert ['script','script'] == [s for s in script_generator(2)]
+    assert ['script', 'script'] == [s for s in script_generator(2)]
